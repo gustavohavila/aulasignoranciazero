c12ae414e620b45ac05785c62c1c626bc54e39ea
Gustavo Havilá <gustavohavila@gmail.com>
2025-11-07 14:48:30 -0300

    estudo dia 07/11

---
+++ aula_27.py
@@ -0,0 +1,30 @@
+a=10
+b=a
+a=1
+a
+1
+b
+10
+a=100
+a.bit_lenth()
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    a.bit_lenth()
+AttributeError: 'int' object has no attribute 'bit_lenth'. Did you mean: 'bit_length'?
+a.bit_lengh()
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    a.bit_lengh()
+AttributeError: 'int' object has no attribute 'bit_lengh'. Did you mean: 'bit_length'?
+a.bit.length()
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    a.bit.length()
+AttributeError: 'int' object has no attribute 'bit'
+a.bit_lengh()
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    a.bit_lengh()
+AttributeError: 'int' object has no attribute 'bit_lengh'. Did you mean: 'bit_length'?
+a.bit_length()
+7
